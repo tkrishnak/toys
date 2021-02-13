f5a81523b2917a4da8591ba21e1e5a945b2820c3
tkrishnak <tkrishnagokul@gmail.com>
2021-02-13 17:57:01 -0500

    Initial code base

---
+++ src/main/java/com/shopping/toys/ToysApplication.java
@@ -0,0 +1,13 @@
+package com.shopping.toys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToysApplication.class, args);
+	}
+
+}
